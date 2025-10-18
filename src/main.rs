94d0bba920fe0ee14bb3d8e476474f9861f674de
Amir Mohammadi <amiremohamadi@yahoo.com>
2025-10-18 12:56:46 +0330

    init cli

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,24 @@
 mod engine;
 mod parser;
 
-fn main() {
-    println!("Hello, world!");
+use anyhow::Result;
+use clap::Parser;
+use engine::Engine;
+
+#[derive(Debug, Parser)]
+#[clap(author, version)]
+pub struct Args {
+    pub file: String,
+}
+
+fn main() -> Result<()> {
+    let args = Args::parse();
+    let file = match std::fs::read_to_string(&args.file) {
+        Ok(c) => c,
+        _ => panic!("could not find the file '{}'", &args.file),
+    };
+
+    let engine = Engine::init(&file)?;
+
+    Ok(())
 }
